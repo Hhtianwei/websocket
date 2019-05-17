d551d4b5f09bbf6c57f01f3e21ba9e7d16f7b56f
TianWeiSH <TianWeiSH@nipponpaint.com.cn>
2019-05-17 11:10:47 +0800

    more detail

--- src/main/java/com/training/demo0515/config/WebSocketConfig.java
+++ src/main/java/com/training/demo0515/config/WebSocketConfig.java
@@ -1,32 +1,14 @@
 package com.training.demo0515.config;
 
-import com.training.demo0515.handler.MyHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.CloseStatus;
-import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.WebSocketMessage;
-import org.springframework.web.socket.WebSocketSession;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+    public class WebSocketConfig {
 
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(new MyHandler(), "/myHandler").setAllowedOrigins("*");
-    }
-
-    @Bean
-    public ServletServerContainerFactoryBean createWebSocketContainer() {
-        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
-        container.setMaxTextMessageBufferSize(8192);
-        container.setMaxBinaryMessageBufferSize(8192);
-        return container;
-    }
-
+        @Bean
+        public ServerEndpointExporter serverEndpointExporter() {
+            return new ServerEndpointExporter();
+        }
 }
